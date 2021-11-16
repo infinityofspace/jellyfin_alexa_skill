31147ddb2914663e924647919f13b5afbcb1c191
infinityofspace <30715462+infinityofspace@users.noreply.github.com>
2021-11-16 18:51:18 +0100

    added unittests for jellyfin api client

---
+++ tests/test_jellyfin_api_client.py
@@ -0,0 +1,19 @@
+import unittest
+
+from jellyfin.api.client import JellyfinClient
+
+
+class TestJellyfinApiClient(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.client = JellyfinClient(server_endpoint="http://localhost:8096")
+
+    def test_connection(self):
+        info = self.client.public_info()
+
+        self.assertIsNotNone(info)
+
+
+if __name__ == "__main__":
+    unittest.main()
