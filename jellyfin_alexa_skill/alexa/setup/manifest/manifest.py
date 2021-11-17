f504ec3bb8b68251a255a414b00874242726cc36
infinityofspace <30715462+infinityofspace@users.noreply.github.com>
2021-11-17 21:19:09 +0100

    automatically generate manifest version suffix

--- jellyfin_alexa_skill/alexa/setup/manifest/manifest.py
+++ jellyfin_alexa_skill/alexa/setup/manifest/manifest.py
@@ -3,12 +3,16 @@ import importlib.resources as pkg_resources
 from ask_sdk_core.serialize import DefaultSerializer
 from ask_smapi_model.v1.skill.manifest import SkillManifestEnvelope
 
+from jellyfin_alexa_skill import __version__
 from jellyfin_alexa_skill.alexa.setup import manifest as manifest_module
 
 BASE_NAME = "jellyfin"
 
-SKILL_MANIFEST = DefaultSerializer().deserialize(pkg_resources.read_text(manifest_module, "manifest.json"),
-                                                 SkillManifestEnvelope)
+_manifest = DefaultSerializer().deserialize(pkg_resources.read_text(manifest_module, "manifest.json"),
+                                            SkillManifestEnvelope)
+_manifest.manifest.publishing_information.name = _manifest.manifest.publishing_information.name + "_" + __version__
+
+SKILL_MANIFEST = _manifest
 
 
 def get_skill_version(manifest: SkillManifestEnvelope) -> str:
