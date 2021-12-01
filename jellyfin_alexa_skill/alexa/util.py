5c690077084da0d3d3bbe5028409368471ce1ecc
Yahoo Mike <ya_mikew@yahoo.com.au>
2021-12-01 16:33:14 +1100

    add backend for video support to launch VideoApp

--- jellyfin_alexa_skill/alexa/util.py
+++ jellyfin_alexa_skill/alexa/util.py
@@ -1,3 +1,4 @@
+import logging
 from difflib import SequenceMatcher
 from enum import Enum
 from functools import wraps
@@ -5,6 +6,8 @@ from random import shuffle
 
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model.interfaces.audioplayer import PlayDirective, PlayBehavior, AudioItem, Stream, AudioItemMetadata
+from ask_sdk_model.interfaces.videoapp import LaunchDirective, VideoItem, Metadata
+
 from ask_sdk_model.interfaces.display import Image, ImageInstance
 
 from jellyfin_alexa_skill.config import get_translation
@@ -37,30 +40,45 @@ def build_stream_response(jellyfin_client: JellyfinClient,
     else:
         item = playback.queue[idx]
 
-    stream_url = jellyfin_client.get_stream_url(item_id=item["id"], user_id=jellyfin_user_id, token=jellyfin_token)
+    stream = jellyfin_client.get_stream_url(item_id=item["id"], user_id=jellyfin_user_id, token=jellyfin_token)
+    stream_type = stream[0]
+    stream_url = stream[1]
 
-    art_image = None
-    art_url = jellyfin_client.get_art_url(item_id=item["id"], token=jellyfin_token)
-    if art_url:
-        art_image = Image( sources=[ImageInstance(url=art_url)] )
+    if stream_type == "audio":
+        art_image = None
+        art_url = jellyfin_client.get_art_url(item_id=item["id"], token=jellyfin_token)
+        if art_url:
+            art_image = Image( sources=[ImageInstance(url=art_url)] )
 
-    handler_input.response_builder.add_directive(
-        PlayDirective(
-            play_behavior=PlayBehavior.REPLACE_ALL,
-            audio_item=AudioItem(
-                stream=Stream(
-                    token=item["id"],
-                    url=stream_url,
-                    offset_in_milliseconds=0),
-                metadata=AudioItemMetadata(
-                    title=item["title"],
-                    subtitle=" ,".join(item["artists"]),
-                    art=art_image
+        handler_input.response_builder.add_directive(
+            PlayDirective(
+                play_behavior=PlayBehavior.REPLACE_ALL,
+                audio_item=AudioItem(
+                    stream=Stream(
+                        token=item["id"],
+                        url=stream_url,
+                        offset_in_milliseconds=0),
+                    metadata=AudioItemMetadata(
+                        title=item["title"],
+                        subtitle=" ,".join(item["artists"]),
+                        art=art_image
+                    )
                 )
             )
         )
-    )
-
+    elif stream_type == "video":
+        handler_input.response_builder.add_directive(
+            LaunchDirective(
+                video_item=VideoItem(
+                    source=stream_url,
+                    metadata=Metadata(
+                        title=item["title"]
+                    )
+                )
+            )
+        )
+    else:
+        logging.warning("Unknown stream_type [%s]",stream_type)
 
 def translate(func):
     @wraps(func)
