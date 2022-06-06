a1056abe1e8bc013df6ac53eeb295c5e7408411c
rikman122 <rikipr_12@hotmail.com>
2022-06-06 10:51:05 +0200

    shuffle mode

--- jellyfin_alexa_skill/alexa/handler/__init__.py
+++ jellyfin_alexa_skill/alexa/handler/__init__.py
@@ -12,6 +12,7 @@ from jellyfin_alexa_skill.alexa.handler.interface import *
 from jellyfin_alexa_skill.alexa.handler.launch import *
 from jellyfin_alexa_skill.alexa.handler.playlist import *
 from jellyfin_alexa_skill.alexa.handler.yesno import *
+from jellyfin_alexa_skill.alexa.handler.shuffle import *
 
 
 def get_skill_builder(jellyfin_client: JellyfinClient):
@@ -61,5 +62,7 @@ def get_skill_builder(jellyfin_client: JellyfinClient):
     skill_builder.add_request_handler(HelpIntentHandler())
 
     skill_builder.add_request_handler(YesNoIntentHandler(jellyfin_client))
+
+    skill_builder.add_request_handler(ShuffleIntentHandler(jellyfin_client))
 
     return skill_builder
