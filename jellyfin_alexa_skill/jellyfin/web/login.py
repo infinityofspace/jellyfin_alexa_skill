79c8a0596b62b620b1190acb720102bcbd4b77cd
infinityofspace <30715462+infinityofspace@users.noreply.github.com>
2022-07-26 12:19:28 +0200

    adjusted login flow

--- jellyfin_alexa_skill/jellyfin/web/login.py
+++ jellyfin_alexa_skill/jellyfin/web/login.py
@@ -17,67 +17,80 @@ templates_path = Path(__file__).parent.resolve() / "templates"
 def get_jellyfin_login_blueprint(jellyfin_server_endpoint: str, client_id: str):
     login_blueprint = Blueprint("login", __name__, template_folder=str(templates_path))
 
-    @login_blueprint.route("/login", methods=["GET", "POST"])
-    def login():
+    @login_blueprint.route("/login", methods=["GET"])
+    def login_get():
         if client_id != request.args.get("client_id", None):
             return abort(401)
 
-        redirect_uri = request.args.get("redirect_uri", "")
-        if not redirect_uri:
-            redirect_uri = request.form["redirect_uri"]
+        redirect_uri = request.args.get("redirect_uri", None)
+        if not redirect_uri or not VALID_ALEXA_REDIRECT_URLS_REGEX.match(redirect_uri):
+            return abort(400)
 
         state = request.args.get("state", None)
         if not state:
-            state = request.form["state"]
-
-        if not VALID_ALEXA_REDIRECT_URLS_REGEX.match(redirect_uri) or not state:
             return abort(400)
 
-        if request.method == "POST":
-            client = JellyfinClient(jellyfin_server_endpoint)
+        return render_template("login.html",
+                               redirect_uri=redirect_uri,
+                               client_id=client_id,
+                               state=state)
 
-            try:
-                user_id, token = client.get_auth_token(username=request.form["username"],
-                                                       password=request.form["password"])
-            except HTTPError as e:
-                if e.response.status_code == 401:
-                    return render_template("login.html",
-                                           redirect_uri=redirect_uri,
-                                           state=state,
-                                           error="Invalid username or password")
-                else:
-                    return render_template("login.html",
-                                           redirect_uri=redirect_uri,
-                                           state=state,
-                                           error="Something went wrong while accessing your Jellyfin server")
-            except ConnectionError as e:
+    @login_blueprint.route("/login", methods=["POST"])
+    def login_post():
+        if client_id != request.form.get("client_id", None):
+            return abort(401)
+
+        redirect_uri = request.form.get("redirect_uri", None)
+        if not redirect_uri or not VALID_ALEXA_REDIRECT_URLS_REGEX.match(redirect_uri):
+            return abort(400)
+
+        state = request.form.get("state", None)
+        if not state:
+            return abort(400)
+
+        client = JellyfinClient(jellyfin_server_endpoint)
+
+        try:
+            user_id, token = client.get_auth_token(username=request.form["username"],
+                                                   password=request.form["password"])
+        except HTTPError as e:
+            if e.response.status_code == 401:
                 return render_template("login.html",
                                        redirect_uri=redirect_uri,
                                        state=state,
-                                       error="Cant reach your Jellyfin server. Please check your firewall "
-                                             "and allow external access to your Jellyfin server")
-            except Exception as e:
-                logging.error(e)
-                return abort(500)
-
-            alexa_auth_token = binascii.hexlify(os.urandom(64)).decode("utf8")
-
-            user = User.create(alexa_auth_token=alexa_auth_token,
-                               jellyfin_user_id=user_id,
-                               jellyfin_token=token)
-            user.save()
-
-            params = {
-                "access_token": alexa_auth_token,
-                "state": state,
-                "token_type": "token"
-            }
-
-            return redirect(redirect_uri + "#" + urllib.parse.urlencode(params),
-                            code=302)
-        else:
+                                       client_id=client_id,
+                                       error="Invalid username or password")
+            else:
+                return render_template("login.html",
+                                       redirect_uri=redirect_uri,
+                                       state=state,
+                                       client_id=client_id,
+                                       error="Something went wrong while accessing your Jellyfin server")
+        except ConnectionError as e:
             return render_template("login.html",
                                    redirect_uri=redirect_uri,
-                                   state=state)
+                                   state=state,
+                                   client_id=client_id,
+                                   error="Cant reach your Jellyfin server. Please check your firewall "
+                                         "and allow external access to your Jellyfin server")
+        except Exception as e:
+            logging.error(e)
+            return abort(500)
+
+        alexa_auth_token = binascii.hexlify(os.urandom(64)).decode("utf8")
+
+        user = User.create(alexa_auth_token=alexa_auth_token,
+                           jellyfin_user_id=user_id,
+                           jellyfin_token=token)
+        user.save()
+
+        params = {
+            "access_token": alexa_auth_token,
+            "state": state,
+            "token_type": "token"
+        }
+
+        return redirect(redirect_uri + "#" + urllib.parse.urlencode(params),
+                        code=302)
 
     return login_blueprint
